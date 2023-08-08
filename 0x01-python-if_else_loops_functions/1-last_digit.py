f73c7b4493fd825e7c96bc0ef99da4cb37fdccfb
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-08-08 13:49:44 +0300

    Fixing the output case when a number is negative

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,11 +3,12 @@ import random
 number = random.randint(-10000, 10000)
 
 if number < 0:
-    last_digit = -(number) % 10
+    last_digit = -(-(number) % 10)
 else:
     last_digit = number % 10
 
 message = "Last digit of {} is {}".format(number, last_digit)
+
 if last_digit > 5:
     message += " and is greater than 5"
 elif last_digit == 0:
