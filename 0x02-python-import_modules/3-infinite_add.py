e1fb1eaea6183c67696999cf62b473a42c894db1
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-08-10 18:17:48 +0300

    Fixing a logical error in the calculation

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     args = argv[1:]
 
     sum_no = 0
-    for i in range(len(args) - 1):
+    for i in range(len(args)):
         arg = args[i]
         sum_no += int(arg)
     print(sum_no)
