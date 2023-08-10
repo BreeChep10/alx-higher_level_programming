03694ddc2ad718053efda763d3024bb53ace1a3d
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-08-10 18:22:48 +0300

    checker fix

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    import hidden_4
-
     """printing all the names defined in the module"""
+    import hidden_4
 
     names = dir(hidden_4)
     for i in names:
