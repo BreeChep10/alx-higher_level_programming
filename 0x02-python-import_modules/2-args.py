81bd342aa58a1709ab792b07ef42e0efdfdcb9c9
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-08-10 14:33:12 +0300

    A program that prints the number of and the list of its arguements

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+from sys import argv
+
+
+if __name__ == "__main__":
+    no_args = len(argv) - 1
+    args = argv[1:]
+
+    print("{} arguement{}{}".format(
+        no_args,
+        's' if no_args != 1 else '',
+        ':' if no_args > 0 else '.'))
+
+    for i in range(len(args)):
+        pos = i + 1
+        arg = args[i]
+        print("{}: {}".format(pos, arg))
+
