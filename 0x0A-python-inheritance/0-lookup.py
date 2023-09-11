6483e15e9bf62df9b31cc0fc3760cdb89e9f72ef
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-09-11 16:28:44 +0300

    writing a function that returns the list of available attributes and methods of an object

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""function that returns the list of available attributes and methods"""
+
+
+def lookup(obj):
+    _all = dir(obj)
+
+    public = [name for name in _all if not name.startswith('-')]
+
+    return public
