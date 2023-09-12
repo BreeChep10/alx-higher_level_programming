1db351ff990bfb02a76512976e35cff013d8d25d
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-09-12 20:49:50 +0300

    class to json

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+function that returns the dictionary description.
+"""
+
+
+def class_to_json(obj):
+    """
+    initialize an empty dictionary to store the json rep.
+    """
+    json_dict = {}
+
+    for name in dir(obj):
+        if not name.startswith('-'):
+            value = getattr(obj, name)
+
+            """
+            check if the attribute is a serializable data type.
+            """
+            if isinstance(value, (list, dict, str, int, bool)):
+                json_dict[name] = value
+    """
+    returning the json dictionary
+    """
