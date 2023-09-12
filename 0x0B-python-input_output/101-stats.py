21c6369ccc52f3e057f35f72451ac0929ac0cc43
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-09-13 00:01:57 +0300

    advanced

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,62 +1,59 @@
-import sys
+#!/usr/bin/python3
+"""Reads from standard input and computes metrics.
 
+After every 10 lines or the input of a keyboard interruption (CTRL + C),
+prints the following statistics:
+    - Total file size up to that point.
+    - Count of read status codes up to that point.
 """
-Initialize dictionaries to store file sizes and status code counts
-"""
 
 
-file_sizes = {}
-status_code_counts = {}
+def print_stats(size, status_codes):
+    """Print accumulated metrics.
 
-"""
-Initialize variables to keep track of total file size and line count
-"""
-total_size = 0
-line_count = 0
+    Args:
+        size (int): The accumulated read file size.
+        status_codes (dict): The accumulated count of status codes.
+    """
+    print("File size: {}".format(size))
+    for key in sorted(status_codes):
+        print("{}: {}".format(key, status_codes[key]))
 
-try:
-    for line in sys.stdin:
-        line_count += 1
-        parts = line.split()
-        
-        """
-        Check if the line has the expected format
-        """
-        if len(parts) >= 9:
-            status_code = parts[-2]
-            size = parts[-1]
-            
-            """
-            Update total file size
-            """
-            total_size += int(size)
-            
-            """
-            Update file size dictionary
-            """
-            file_sizes[line_count] = total_size
-            
-            """
-            Update status code counts dictionary
-            """
-            if status_code in status_code_counts:
-                status_code_counts[status_code] += 1
+
+if __name__ == "__main__":
+    import sys
+
+    size = 0
+    status_codes = {}
+    valid_codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    count = 0
+
+    try:
+        for line in sys.stdin:
+            if count == 10:
+                print_stats(size, status_codes)
+                count = 1
             else:
-                status_code_counts[status_code] = 1
-            
-            """
-            Print statistics every 10 lines
-            """
-            if line_count % 10 == 0:
-                print("Total file size:", total_size)
-                for code in sorted(status_code_counts.keys()):
-                    print(f"{code}: {status_code_counts[code]}")
+                count += 1
 
-except KeyboardInterrupt:
-    """
-    Handle KeyboardInterrupt (CTRL + C)
-    """
-    print("Total file size:", total_size)
-    for code in sorted(status_code_counts.keys()):
-        print(f"{code}: {status_code_counts[code]}")
+            line = line.split()
 
+            try:
+                size += int(line[-1])
+            except (IndexError, ValueError):
+                pass
+
+            try:
+                if line[-2] in valid_codes:
+                    if status_codes.get(line[-2], -1) == -1:
+                        status_codes[line[-2]] = 1
+                    else:
+                        status_codes[line[-2]] += 1
+            except IndexError:
+                pass
+
+        print_stats(size, status_codes)
+
+    except KeyboardInterrupt:
+        print_stats(size, status_codes)
+        raise
