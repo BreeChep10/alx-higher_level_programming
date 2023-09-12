5cac5276c339a0b0879b658fef46a798d45a215e
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2023-09-12 20:54:43 +0300

    class to json

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -7,20 +7,6 @@ function that returns the dictionary description.
 
 def class_to_json(obj):
     """
-    initialize an empty dictionary to store the json rep.
+    returns a python dictionary with a simple data structure.
     """
-    json_dict = {}
-
-    for name in dir(obj):
-        if not name.startswith('-'):
-            value = getattr(obj, name)
-
-            """
-            check if the attribute is a serializable data type.
-            """
-            if isinstance(value, (list, dict, str, int, bool)):
-                json_dict[name] = value
-    """
-    returning the json dictionary
-    """
-    return json_dict
+    return obj.__dict__
