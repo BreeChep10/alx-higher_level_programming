24535eb22ea4f302f6cd60a77608ee89109f5990
Bridgit Chepkwony <bridgitchepkwony3@gmail.com>
2024-01-30 21:53:17 +0300

    webscrapping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,22 +1,5 @@
 #!/usr/bin/node
-
 const fs = require('fs');
-
-const readAndPrintFileContent = (filePath) => {
-  try {
-    const content = fs.readFileSync(filePath, 'utf-8');
-    console.log(content);
-  } catch (error) {
-    console.error(`An error occurred: ${error.message}`);
-  }
-};
-
-/** Check if a file path is provided as a command-line argument **/
-
-if (process.argv.length !== 3) {
-  console.error('Usage: ./script.js <file_path>');
-  process.exit(1);
-}
-
-const filePath = process.argv[2];
-readAndPrintFileContent(filePath);
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+  console.log(error || content);
+});
